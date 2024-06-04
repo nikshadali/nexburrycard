becf27b8ab4ac9af01d4cd346493096787cfd7e4
nikshadali <nikali92100@gmail.com>
2024-06-04 11:28:35 +0500

    format code

--- src/App.jsx
+++ src/App.jsx
@@ -8,23 +8,26 @@ function App() {
  
   const [cardItems, setCardItems] = useState(products)
   
+  //call useEffect hook
   useEffect(() => {
     setCardItems(products)
   },[])
    
+  // create increseQty function
   let increseQty = (id) => {
     setCardItems(items => items.map(item => item.id === id ? {...item, qty: item.qty + 1} : item))
+}
 
-  }
-
-  let cartItem = cardItems.reduce((acc, val) => val.qty + acc,0)
+  // get item qty 
+ let cartItem = cardItems.reduce((acc, val) => val.qty + acc,0)
+ 
+ // get Total Amount 
  let totalPrice = cardItems.reduce((acc, val) => (val.qty * val.price) + acc, 0)
   
- console.log(totalPrice)
+ 
  return (
     <>
-   <Header cartItem= {cartItem} totalPrice={totalPrice} />
-    
+    <Header cartItem= {cartItem} totalPrice={totalPrice} />
     <CardList products = {cardItems} increseQty= {increseQty}  />
     </>
   )
